47098d1a99eca87d2fc0454035f1da5b26b11798
soida <adioskensunny@gmail.com>
2025-07-11 09:14:18 +0800

    v0.2

--- index.14622.js
+++ index.14622.js
@@ -1,4 +1,4 @@
-System.register(["./application.96cf6.js"], function (_export, _context) {
+System.register(["./application.2f1bd.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
