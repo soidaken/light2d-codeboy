4f7f03a914b0cc4432383a8839acf78329eb70cc
soida <adioskensunny@gmail.com>
2025-07-11 22:46:34 +0800

    v0.2.1

--- application.67d4c.js
+++ application.67d4c.js
@@ -14,7 +14,7 @@ System.register([], function (_export, _context) {
       _export("Application", Application = /*#__PURE__*/function () {
         function Application() {
           _classCallCheck(this, Application);
-          this.settingsPath = 'src/settings.71026.json';
+          this.settingsPath = 'src/settings.9f0c1.json';
           this.showFPS = false;
         }
         _createClass(Application, [{
--- index.563d5.js
+++ index.563d5.js
@@ -1,4 +1,4 @@
-System.register(["./application.2f1bd.js"], function (_export, _context) {
+System.register(["./application.67d4c.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
